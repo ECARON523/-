fdf3d3a727132f56ba71528b4a7db05b09eb551f
kh9do <135738234+ECARON523@users.noreply.github.com>
2026-04-07 13:24:39 +0000

    МЕГАЛАСТ ВЕРСИЯ 11 ЛАБА

--- smart-dashboard/server/server.js
+++ smart-dashboard/server/server.js
@@ -28,7 +28,7 @@ app.use("/api/notes", noteRoutes);
 app.use("/api/leaderboard", leaderboardRoutes);
 
 // ВАЖНО: Если юзер обновит страницу не на корне, отдаем index.html (для SPA)
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
     // Если запрос не к API, отдаем главную страницу
     if (!req.path.startsWith('/api')) {
         res.sendFile(path.join(__dirname, '../public/index.html'));
