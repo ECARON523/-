e4d5d8b194460df7945601d4011a1cf64240d492
kh9do <135738234+ECARON523@users.noreply.github.com>
2026-04-07 09:24:55 +0000

    Фикса 4

--- smart-dashboard/src/core/router.js
+++ smart-dashboard/src/core/router.js
@@ -1,36 +1,31 @@
 let routes = {};
-let defaultRoute = null;
+let defaultRoute = "/tasks";
 
-// Инициализация маршрутов
 export function initRouter() {
-  // Определяем маршруты и их обработчики
-  // src/core/router.js
-    const routes = {
-    "/tasks": () => import("../modules/tasks/tasksUI.js").then(m => m.renderTasksUI()),
-    "/notes": () => import("../modules/notes/notesUI.js").then(m => m.renderNotesUI()),
-    "/tracker": () => import("../modules/tracker/trackerUI.js").then(m => m.renderTrackerUI())
-    };
+  // Теперь здесь функции, которые возвращают промис
+  routes = {
+    "/tasks": () => import("../modules/tasks/tasksUI.js").then(mod => mod.renderTasksUI()),
+    "/notes": () => import("../modules/notes/notesUI.js").then(mod => mod.renderNotesUI()),
+    "/tracker": () => import("../modules/tracker/trackerUI.js").then(mod => mod.renderTrackerUI())
+  };
 
-// ... остальной код (handleRoute и т.д.)
-
-  defaultRoute = "/tasks"; // По умолчанию открывается Tasks
-
-  // Слушаем изменения URL (кнопки браузера)
   window.addEventListener("popstate", handleRoute);
-
-  // Инициализация маршрута при загрузке
   handleRoute();
 }
 
-// Переход по маршруту
 export function navigate(path) {
-  history.pushState({}, "", path);
+  window.history.pushState({}, "", path);
   handleRoute();
 }
 
-// Обработка маршрута
-function handleRoute() {
+async function handleRoute() {
   const path = window.location.pathname;
   const route = routes[path] ? path : defaultRoute;
-  routes[route]();
+  
+  // Добавляем await, чтобы дождаться загрузки модуля
+  try {
+    await routes[route]();
+  } catch (err) {
+    console.error("Ошибка загрузки модуля:", err);
+  }
 }
